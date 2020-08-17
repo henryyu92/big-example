6a7ce20a2bc0950aca153122eea2d895e1d20c58
henryyu92 <1401802687@qq.com>
2020-08-17 22:48:23 +0800

    update

--- big-es/script/single.sh
+++ big-es/script/single.sh
@@ -10,7 +10,6 @@ docker run \
 -v /data/elasticsearch:/usr/share/elasticsearch/data \
 -p 9200:9200 -p 9300:9300 \
 -ulimits memlock.soft=-1 memlock.hard=-1
-
 -d \
 docker.elastic.co/elasticsearch/elasticsearch:7.8.1
 
